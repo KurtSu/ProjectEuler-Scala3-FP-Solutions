99728c3b858688709a044546feff4cf4b968216e
Kurt Su <programkurt@gmail.com>
2023-08-04 21:44:25 -0500

    mod main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,11 +2,14 @@ package net.projecteuler.kurtsu
 
 @main
 def main(): Unit = {
-  val solutions: List[Solution] = Problem40 ::
+  lazy val solutions: List[Solution] = Problem40 ::
     Problem73 ::
-    Problem89 ::
+    Problem89 :: Problem90 ::
     Nil
 
+  lazy val problemNumNotToInclude = List(89)
+  
   solutions
+    .filter(x => !problemNumNotToInclude.contains(x.problemNum))
     .foreach(x => println(s"problem ${x.problemNum}: ${x.solution()}"))
 }
